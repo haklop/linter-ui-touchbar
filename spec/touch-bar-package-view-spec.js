0db88d5b31aa298a3cb6f3a89b078d9ad7073006
Eric Bellemon <eric.bellemon@radiofrance.com>
2017-07-13 23:09:05 +0200

    init

---
+++ spec/touch-bar-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TouchBarPackageView from '../lib/touch-bar-package-view';
+
+describe('TouchBarPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
